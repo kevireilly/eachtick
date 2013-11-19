1829690162a2b0f18e4578e4b00866204f5d8417
Kevin Reilly <kevin@vishun.net>
2013-11-18 18:47:39 -0800

    Improved tick method setting and completion returning

--- eachtick.js
+++ eachtick.js
@@ -10,6 +10,23 @@
  *     for Node.js and the browser.
  *  -----------------------------------
  *
+ *  Set the tick method based on availability
+ *
+ * @returns {*}
+ *   Instance of available tick method
+ */
+function settick() {
+  return (typeof process !== 'undefined' && process.nextTick) ? process.nextTick
+    : (typeof setImmediate !== 'undefined') ? setImmediate
+    : setTimeout;
+}
+// Get and set the tick method immediately
+var _tick = _initial = settick();
+
+/**
+ *
+ * Perform iterations on each tick
+ *
  * @param obj
  *   Object or array to be iterated over
  * @param iterator
@@ -20,14 +37,17 @@
  */
 function eachtick(obj, iterator, complete){
   var keys = Object.keys(obj);
+
+  // Without this, a large iteration causes
+  // "Maximum call stack size exceeded" errors
+  _tick = (isNode && keys.length > 999) ? setImmediate : _initial;
+
   (function iterate(keys){
     nexttick(function(){
       // Return the key / value pair, wait for next callback
       iterator(keys[0], obj[keys[0]], function next(err, stop){
-        // Stop iterating on error or stop instruction
-        if (err || stop) return complete ? complete(err, stop) : false;
-        // Check if we're done
-        if (keys.length === 1) return complete ? complete() : true;
+        // Stop iterating on error, stop instruction, or completion
+        if (err || stop || keys.length === 1) return complete ? complete(err, stop) : false;
         // Grab a slice to go. Pepperoni please.
         return ((keys = keys.slice(1)).length && iterate(keys));
       });
@@ -37,22 +57,24 @@ function eachtick(obj, iterator, complete){
 
 /**
  *
- * Cross-platform tick
+ * Call the tick method
  *
  * @param fn
  *   Callback for each tick
  * @returns {*}
  *   Cached tick method
  */
-function nexttick(fn) { return tick(fn, 0); }
-var tick = (typeof process !== 'undefined' && process.nextTick) ? process.nextTick
-  : (typeof setImmediate !== 'undefined') ? setImmediate
-  : setTimeout;
-
+function nexttick(callback) {
+  return _tick(callback, 0);
+}
 
 /**
- * Cross-platform exports
+ *
+ * Node.js exports
+ *
  */
+var isNode = false;
 if (typeof module !== 'undefined') {
+  isNode = true;
   module.exports = eachtick;
 }
